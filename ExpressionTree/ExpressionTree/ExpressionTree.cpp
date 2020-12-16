0d2074d5a1d4677c80dd971ae1bfc6da1ceb0611
Zaryab <imzaryab@gmail.com>
2020-12-16 19:37:26 +0500

    Infix to Postfix Conversion Deployed

--- ExpressionTree/ExpressionTree/ExpressionTree.cpp
+++ ExpressionTree/ExpressionTree/ExpressionTree.cpp
@@ -1,20 +1,181 @@
-// ExpressionTree.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main()
+class node
 {
-    std::cout << "Hello World!\n";
+public:
+	char data;
+	node* next;
+};
+
+
+class Stack
+{
+	node* top = NULL;
+
+public:
+	void Push(char newelement);
+	char Pop();
+	bool IsEmpty();
+	void PopDiscard();
+	char Top();
+
+};
+
+bool Stack::IsEmpty()
+{
+	if (top == NULL)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void Stack::Push(char newelement)
+{
+	node* newptr;
+	newptr = new node;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	newptr->data = newelement;
+	newptr->next = top;
+
+	top = newptr;
+}
+
+char Stack::Pop()
+{
+	char returnValue;
+
+	node* tempptr = top;
+	returnValue = top->data;
+	top = top->next;
+	delete tempptr;
+
+	return returnValue;
+}
+
+void Stack::PopDiscard()
+{
+	node* temp = top;
+	top = top->next;
+	delete temp;
+}
+
+char Stack::Top()
+{
+	if (top)
+	{
+		return top->data;
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+///////////////////////////////////////////////////////////////////////
+//------------------OPERATOR PRECEDENCE FUNCTION--------------------//
+/////////////////////////////////////////////////////////////////////
+
+bool prcd(char a, char b)
+{
+	if ((a == '*' && b == '+') || (a == '*' && b == '-') || (a == '/' && b == '+') || (a == '/' && b == '-') || (a == '-' && b == '+') || (a == '/' && b == '*'))
+		return true;
+	else if ((a == '+' && b == '*') || (a == '+' && b == '/') || (a == '-' && b == '*') || (a == '-' && b == '/') || (a == '+' && b == '-') || (a == '*' && b == '/'))
+		return false;
+	else if ((a == '+' && b == '+') || (a == '-' && b == '-') || (a == '*' && b == '*') || (a == '/' && b == '/'))
+		return true;
+	else if ((a == '+' && b == ')') || (a == '-' && b == ')') || (a == '*' && b == ')') || (a == '/' && b == ')'))
+		return true;
+	else if ((a == '+' && b == '(') || (a == '-' && b == '(') || (a == '*' && b == '(') || (a == '/' && b == '('))
+		return false;
+	else if ((a == '(' && b == '+') || (a == '(' && b == '-') || (a == '(' && b == '*') || (a == '(' && b == '/'))
+		return false;
+	else if ((a == '(' && b == ')'))
+		return false;
+}
+
+///////////////////////////////////////////////////////////////////////
+//--------------------INFIX TO POSTFIX FUNCTION---------------------//
+/////////////////////////////////////////////////////////////////////
+
+string infixTopostfix(string expression)
+{
+	Stack optstck;
+	char symb;
+	vector<char> postString;
+
+	int counter = 0;
+
+	int i = 0;
+	while (expression[i])
+	{
+		symb = expression[i];
+
+		if (symb != '+' && symb != '-' && symb != '*' && symb != '/' && symb != '(' && symb != ')')
+		{
+			postString.push_back(symb);
+			cout << "Operand added..." << endl;
+			counter++;
+		}
+		else
+		{
+			while (!optstck.IsEmpty() && prcd(optstck.Top(), symb))
+			{
+				char topsymb = optstck.Pop();
+				cout << "Operator Popped due to Precedence..." << endl;
+				postString.push_back(topsymb);
+			}
+			if (optstck.IsEmpty() || symb != ')')
+			{
+				cout << "Operator Pushed to Stack..." << endl;
+				optstck.Push(symb);
+				counter++;
+			}
+			else
+			{
+				optstck.PopDiscard();
+				counter--;
+			}
+		}
+
+		i++;
+	}
+
+	//output remaining operators
+	while (!optstck.IsEmpty())
+	{
+		char topsymb = optstck.Pop();
+		postString.push_back(topsymb);
+	}
+
+	//Shift array to string for Return
+	string postfixExp;
+	
+	for(int i = 0; i < counter; i++)
+	{
+		postfixExp = postfixExp + postString[i];
+	}
+
+	return postfixExp;
+}
+
+void main()
+{
+	string exp = "(5+3)*4";
+
+	cout << exp << endl << endl;
+
+	string postExp = infixTopostfix(exp);
+	cout << endl;
+
+	cout << postExp << endl << endl;
+
+
+
+	system("pause");
+}
